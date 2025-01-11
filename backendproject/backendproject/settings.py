638984a48d1b8c661dec1603bc2dc3822b5bb0f6
lochana14 <152413744+lochana14@users.noreply.github.com>
2025-01-11 16:42:51 +0530

    sixth  commit7

--- backendproject/backendproject/settings.py
+++ backendproject/backendproject/settings.py
@@ -126,8 +126,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://frontend-xgc5.onrender.com"
-
-]
+CORS_ALLOW_ALL_ORIGINS = True
--- backendproject/users/views.py
+++ backendproject/users/views.py
@@ -1,9 +1,4 @@
-from django.shortcuts import render
-
 from django.http import JsonResponse
 
 def get_message(request):
-    data = {
-        'message': 'Hello from Django!'
-    }
-    return JsonResponse(data)
+    return JsonResponse({"message": "Hello from Django!"})
