ff98f5001259aa8ef36a53f8093b7fd9129af3fb
lochana14 <152413744+lochana14@users.noreply.github.com>
2025-01-11 16:05:37 +0530

    third  commit

--- backendproject/users/urls.py
+++ backendproject/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/message/', views.get_message, name='get_message'),
+    path('', views.get_message, name='get_message'),
 ]
