3167b798dbaf2eae051f78a0c274597826093875
lochana14 <152413744+lochana14@users.noreply.github.com>
2025-01-11 17:01:37 +0530

    sixth  comm

--- backendproject/backendproject/settings.py
+++ backendproject/backendproject/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = ['https://frontend-xgc5.onrender.com',
+    'http://localhost:3000',]
+
+CSRF_TRUSTED_ORIGINS = ['https://frontend-xgc5.onrender.com',
+'http://localhost:3000','http://localhost:8000','https://sample1-eq07.onrender.com']
